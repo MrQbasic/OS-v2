40693d4359fd021e23e98121a480bcb30a747056
MrQbasic <leon@arch.localdomain>
2022-03-22 16:18:12 +0100

    AHCI

--- lmake/run.sh
+++ lmake/run.sh
@@ -1,5 +1,11 @@
 cd ..
 rm ./log.txt
 clear
-qemu-system-x86_64 -hda OS.img -cpu max -D log.txt -d int
+qemu-system-x86_64 \
+    -drive id=disk,file=OS.img,if=none,index=0,media=disk,format=raw \
+    -device ahci,id=ahci \
+    -device ide-hd,drive=disk,bus=ahci.0 \
+    -cpu max \
+    -D log.txt \
+    -d int
 cd lmake

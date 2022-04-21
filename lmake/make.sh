27d996ed1aa4b5aa3b69734955c10885d95c6f2a
MrQbasic <leon@arch.localdomain>
2022-04-21 11:30:38 +0200

    make update

--- lmake/make.sh
+++ lmake/make.sh
@@ -1,4 +1,10 @@
+KENEL_BIN=./kernel/kernel.bin
+KENEL_PM_BIN=./kernel/kernel-PM.bin
+BOOT_BIN=./boot/bootsec.bin
+
 cd ..
+rm -rf ./bin
+mkdir ./bin
 clear
 
 cd boot/
@@ -13,9 +19,16 @@ cd ..
 cat boot/bootsec.bin kernel/kernel-PM.bin kernel/kernel.bin 1,44mb.img > tmp.img
 dd if=tmp.img of=OS.img bs=512 count=2880
 
-rm kernel/kernel-PM.bin
-rm kernel/kernel.bin
-rm boot/bootsec.bin
+if [ -f "$KENEL_BIN" ]; then
+    echo "$KENEL_BIN"
+    mv "$KENEL_BIN" ./bin/
+fi
+if [ -f "$KENEL_PM_BIN" ]; then
+    mv "$KENEL_PM_BIN" ./bin/
+fi
+if [ -f "$BOOT_BIN" ]; then
+    mv "$BOOT_BIN" ./bin/
+fi
 rm tmp.img
 
 cd lmake/

b8e496c5b87684ceea96045a1ac29bf5eee9f1e0
MrQbasic <77384846+MrQbasic@users.noreply.github.com>
2022-10-24 01:29:47 +0200

    page map optimizations

--- kernel/memory/page_map.s
+++ kernel/memory/page_map.s
@@ -9,100 +9,88 @@ mem_page_map:
     push rbx
     push rcx
     push rdx
+    push r8   ; page filter buffer
+    push r9   ; buffer for v-addr
+    push r10  ; buffer for p-addr
+    push r11  ; buffer for flags
+    push r12  ; buffer of base addr of ervery stage
+    ;
     ;save the input values
-    mov rsi, mem_page_buffer_p_addr
-    mov [rsi], rax
-    mov rsi, mem_page_buffer_v_addr
-    mov [rsi], rbx
-    mov rsi, mem_page_buffer_flags
-    mov [rsi], cx
-    ;restore if reentry
-    .reentry:
+    mov r10, rax
+    mov r9,  rbx
+    and rcx, 0x0000_0000_0000_00FF
+    mov r11, rcx
     ;setup filter
     mov rsi, V_P_ADDR_BITS
     mov cl, [rsi]
     dec cl
     mov bl, 12
     call math_fill
-    mov rsi, mem_page_filter
-    mov [rsi], rax
+    mov r8, rax
     ;get the PML4 addr from cr3
     mov rdx, cr3
-    mov rsi, mem_page_pml4_base
-    mov [rsi], rdx
+    mov r12, rdx
     ;get pointer to PML4E
-    mov rsi, mem_page_buffer_v_addr
-    mov rbx, [rsi]
+    mov rbx, r9
     mov rdx, 0xFF8000000000
     and rbx, rdx
     shr rbx, 39
     mov rax, 8
     mul rbx
-    mov rsi, mem_page_pml4_base
-    add rax, [rsi]
+    add rax, r12
     mov rbx, [rax]
     ;get the PDPT_base from PML4E
     test rbx, 1
     jz .no_pml4e
-    mov rsi, mem_page_filter
-    and rbx, [rsi]
-    mov rsi, mem_page_pdpt_base
-    mov [rsi], rbx
+    and rbx, r8
+    mov r12, rbx
     ;get the PDPTE
     mov rdx, 0x7FC0000000
-    mov rsi, mem_page_buffer_v_addr
-    mov rbx, [rsi]
+    mov rbx, r9
     and rbx, rdx
     shr rbx, 30
     mov rax, 8
     mul rbx
-    mov rsi, mem_page_pdpt_base
-    add rax, [rsi]
+    add rax, r12
     mov rbx, [rax]
     ;get the PD_base from PDPTE
     test rbx, 1
-    jz .no_pdpt
-    mov rsi, mem_page_filter
-    and rbx, [rsi]
-    mov rsi, mem_page_pd_base
-    mov [rsi], rbx
+    jz .no_pdpte
+    and rbx, r8
+    mov r12, rbx
     ;get the PDE
     mov rdx, 0x3FE00000
-    mov rsi, mem_page_buffer_v_addr
-    mov rbx, [rsi]
+    mov rbx, r9
     and rbx, rdx
     shr rbx, 21
     mov rax, 8
     mul rbx 
-    mov rsi, mem_page_pd_base
-    add rax, [rsi]
+    add rax, r12
     mov rbx, [rax]
     ;get the PT_base from PDE
     test rbx, 1
-    jz .no_pd
-    mov rsi, mem_page_filter
-    and rbx, [rsi]
-    mov rsi, mem_page_pt_base
-    mov [rsi], rbx
+    jz .no_pde
+    and rbx, r8
+    mov r12, rbx
     ;set the PTE
     mov rdx, 0x1FF000
-    mov rsi, mem_page_buffer_v_addr
-    mov rbx, [rsi]
+    mov rbx, r9
     and rbx, rdx
     shr rbx, 12
     mov rax, 8
     mul rbx
-    mov rsi, mem_page_pt_base
-    add rax, [rsi]
+    add rax, r12
     ;-
-    mov rsi, mem_page_buffer_p_addr
-    mov rbx, [rsi]
-    mov rsi, mem_page_filter
-    and rbx, [rsi]
-    mov rdx, [rsi]
-    mov rsi, mem_page_buffer_flags
-    or bl, [rsi]
+    mov rbx, r10
+    and rbx, r8
+    or rbx, r11
     mov [rax], rbx
+    ;exit
+    pop r12
+    pop r11
+    pop r10
+    pop r9
+    pop r8
     pop rdx
     pop rcx
     pop rbx
@@ -113,28 +101,19 @@ mem_page_map:
     .no_pml4e:
         call screen_nl
         call screen_print_no
+        call screen_print_no
+        call screen_print_no
         jmp $
-    .no_pdpt:
+    .no_pdpte:
+        call screen_nl
+        call screen_print_no
+        call screen_print_no
+        jmp $
+    .no_pde:
         call screen_nl
         call screen_print_no
         jmp $
-    .no_pd:
-        call screen_nl
-        call screen_print_no
-        jmp $
 
-mem_page_pml4_base:     dq 0
-mem_page_pdpt_base:     dq 0
-mem_page_pd_base:       dq 0
-mem_page_pt_base:       dq 0
-
-mem_page_res_plm4:      dq 0
-mem_page_res_pdpt:      dq 0
-mem_page_res_pd:        dq 0
-mem_page_res_pt:        dq 0
-
-mem_page_buffer_p_addr: dq 0
-mem_page_buffer_v_addr: dq 0
-mem_page_buffer_flags:  db 0
-
-mem_page_filter:        dq 0
+mem_page_res_plm4e:     dq 0
+mem_page_res_pdpte:     dq 0
+mem_page_res_pde:       dq 0
